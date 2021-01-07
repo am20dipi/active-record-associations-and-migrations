3b00dad50e173aecadc68c928d7a052d950caf58
Adriana <am20dipi@alum.siena.edu>
2021-01-06 19:50:16 -0500

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,11 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
-end
+    def change
+      create_table :artists do |t|
+        t.string :name
+      end
+    end
+  end
+
+  #an artist will have many songs 
+  #an artist will have many genres through songs
+  #through songs means that the table Songs is a JOIN table
--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,13 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
-end
+    def change
+      create_table :songs do |t|
+        t.string :name
+        t.integer :artist_id
+        t.integer :genre_id
+      end
+    end
+  end 
+
+  #since a song belongs to an artist and/or a genre, an instance of a song must have an artist_id and a genre_id.
+  #these are called foreign keys.
+  #table songs is an example of a JOIN table
